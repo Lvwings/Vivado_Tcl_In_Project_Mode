72c4dd2581cb860eeaf45a86648ab1cf8fede573
Lvwings <806764162@qq.com>
2021-11-26 14:37:59 +0800

    - add license

--- tcl/launch.tcl
+++ tcl/launch.tcl
@@ -1,3 +1,25 @@
+# MIT License
+# 
+# Copyright (c) [2021] Lvwings
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # file list
 # work_dir
 #	|-- tcl
@@ -53,7 +75,7 @@ source [file join $tcl_dir proc_update_files.tcl]
 # [-quiet] Ignore command errors (handle no file matches "*.extension" case)
 
 # find_files {base_dir pattern}
-# basedir - the directory to start looking in
+# base_dir- the directory to start looking in
 # pattern - A match pattern, as defined by the glob command
 add_files 				-quiet	[find_files $sources_dir/rtl *.sv]
 add_files 				-quiet	[find_files $sources_dir/rtl *.v]
